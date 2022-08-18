e4cf2adaa22b054e4c93160ed0e9ac22f7651071
Christopher <82610384+GalletaOreo98@users.noreply.github.com>
2022-08-18 00:32:02 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home/home.component';
+import { BooksBarComponent } from './books-bar/books-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BooksBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

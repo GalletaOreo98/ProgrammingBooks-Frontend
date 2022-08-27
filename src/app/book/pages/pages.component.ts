9630a9efe22d4d3ec2eb4ba81cd4ac67d114b6ae
GalletaOreo98 <christophergironcabrera@gmail.com>
2022-08-26 21:59:21 -0600

    UPDATE - Vista previa (modal) para descargar img

--- src/app/book/pages/pages.component.ts
+++ src/app/book/pages/pages.component.ts
@@ -1,7 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IWaifuImage } from 'src/app/models/book-interface';
 import { ProgrammingBooksService } from 'src/app/services/programming-books.service';
+
+@Component({
+  selector: 'ngbd-modal-content',
+  template: `
+    <div class="modal-header container">
+      <h4 class="modal-title">{{name}}</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body container">
+    <img [src]="download_url" class="col-12">
+    </div>
+    <div class="modal-footer">
+      <button class="btn btn-outline-success" (click)="download(download_url)">Download</button>
+      <button type="button" class="btn btn-dark" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `,
+  styleUrls: ['./modal.component.css']
+})
+
+export class NgbdModalContent {
+  
+  @Input() name: string = "";
+  @Input() download_url: string = "";
+
+
+  constructor(public activeModal: NgbActiveModal, private router:Router) {}
+
+  download(url: string){
+    window.open(url);
+  }
+}
 
 @Component({
   selector: 'app-pages',
@@ -11,7 +43,7 @@ import { ProgrammingBooksService } from 'src/app/services/programming-books.serv
 export class PagesComponent implements OnInit {
   bookName: string = "";
   arrayImages!: IWaifuImage[];
-  constructor(private router:Router, private bookService:ProgrammingBooksService) { }
+  constructor(private router:Router, private bookService:ProgrammingBooksService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.bookName = this.router.url;
@@ -19,9 +51,13 @@ export class PagesComponent implements OnInit {
     this.bookService.getBook(this.bookName).subscribe({
       next: (res) => {
         this.arrayImages = res;
-        
       }
     });
   }
 
+  open(name: string, download_url: string) {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.name = name;
+    modalRef.componentInstance.download_url = download_url;
+  }
 }
